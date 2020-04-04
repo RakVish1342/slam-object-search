03a793379d721f404ee64a2219729c727490c5b3
RakVish1342 <rakshith.vishwanatha@gmail.com>
2020-04-04 02:37:16 -0700

    Deleting devel/ . Deleting src/turtlebot3/ and src/turtlebot3_msgs/ so that unreachable (grey double folder) location is removed.

--- catkin_ws/devel/share/turtlebot3_msgs/cmake/turtlebot3_msgs-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(turtlebot3_msgs_MESSAGE_FILES "/root/SES/slam-object-search/catkin_ws/src/old__turtlebot3_msgs/msg/SensorState.msg;/root/SES/slam-object-search/catkin_ws/src/old__turtlebot3_msgs/msg/VersionInfo.msg;/root/SES/slam-object-search/catkin_ws/src/old__turtlebot3_msgs/msg/Sound.msg")
-set(turtlebot3_msgs_SERVICE_FILES "")
