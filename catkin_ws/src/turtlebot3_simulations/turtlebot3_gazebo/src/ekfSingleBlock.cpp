da9b595bed8f5cef8a53f7d015d0dd5c21aa68b0
RakVish1342 <rakshith.vishwanatha@gmail.com>
2020-04-06 13:09:26 -0700

    Included Eigen for matrix manipulation.

--- catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/src/ekfSingleBlock.cpp
+++ catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/src/ekfSingleBlock.cpp
@@ -1,9 +1,13 @@
 #include "/opt/ros/kinetic/include/ros/ros.h"
 #include "/opt/ros/kinetic/include/geometry_msgs/Twist.h"
+// #include "/opt/ros/kinetic/include/eigen_stl_containers/eigen_stl_containers.h"
+#include "Eigen/Dense" // Added include library EIGEN_DIRS=/usr/include/eigen3
 #include <math.h>
 #include <limits>
 #include <nav_msgs/Odometry.h> // Found it using "rostopic info /odom". Is located in /opt/ros/kinetic/include/nav_msgs
 #include <sensor_msgs/LaserScan.h> // Found it using "rostopic info /scan". Is located in /opt/ros/kinetic/include/sensor_msgs
+#include <vector>
+
 
 #define PI 3.14159265
 
@@ -50,55 +54,79 @@ class turtleEkf
 {
 
 private:
-double nextX, prevX;
-double nextY, prevY;
-double nextTh, prevTh; // Angle measured wrt global horizontal frame that gets initialized to
-                                            // Z(into plane), Y(forward direction of init robot pose), X(right of robot)
+    double nextX, prevX;
+    double nextY, prevY;
+    double nextTh, prevTh; // Angle measured wrt global horizontal frame that gets initialized to
+                                                // Z(into plane), Y(forward direction of init robot pose), X(right of robot)
+    float INF; // float type since lidar vals are in float
+    int numModelStates;
+    int numLandmarks;
+    int numStates;
+    std::vector<double> states;
+    Eigen::MatrixXd variances;
 
 public:
-turtleEkf() :
-nextX(0), prevX(0),
-nextY(0), prevY(0),
-nextTh(PI/2.0), prevTh(PI/2.0)
-{
-
-}
-
-~turtleEkf();
-
-// void motionModel()
-void motionModel(double angVel, double linVel, double deltaT)
-{
-  
-    if (angVel > 0.001)
+    turtleEkf() :
+    INF(std::numeric_limits<float>::max()),
+    nextX(0), prevX(0),
+    nextY(0), prevY(0),
+    nextTh(PI/2.0), prevTh(PI/2.0),
+    numModelStates(3),
+    numLandmarks(1),
+    numStates(numModelStates + 2*numLandmarks),
+    states(std::vector<double> (0)), 
+    variances(Eigen::MatrixXd::Zero(numStates, numStates))
     {
-        std::cout << "IF" << std::endl;
-        double r = linVel/angVel;
+        // Set landmark variances to inf
+        variances.bottomRightCorner(numLandmarks, numLandmarks) = Eigen::MatrixXd::Constant(numLandmarks, numLandmarks, INF);
+    };
 
-        //?? ADD other condition of angles 
-        nextX = prevX + ( -r*sin(prevTh) + r*sin(prevTh + angVel*deltaT) );
-        nextY = prevY + ( +r*cos(prevTh) - r*cos(prevTh + angVel*deltaT) );
-        nextTh = prevTh + angVel*deltaT;
-    }
-    else
+    ~turtleEkf() {};
+
+    // void motionModel()
+    std::vector<double> motionModel(double angVel, double linVel, double deltaT)
     {
-        // std::cout << "ELSE" << std::endl;
-        double dist = linVel*deltaT;
+    
+        if (angVel > 0.001)
+        {
+            std::cout << "IF" << std::endl;
+            double r = linVel/angVel;
 
-        // ADD other condition of angles 
-        nextX = prevX - dist*cos(prevTh);
-        nextY = prevY + dist*sin(prevTh);
-        nextTh = prevTh;
-    }
+            //?? ADD other condition of angles 
+            nextX = prevX + ( -r*sin(prevTh) + r*sin(prevTh + angVel*deltaT) );
+            nextY = prevY + ( +r*cos(prevTh) - r*cos(prevTh + angVel*deltaT) );
+            nextTh = prevTh + angVel*deltaT;
+        }
+        else
+        {
+            // std::cout << "ELSE" << std::endl;
+            double dist = linVel*deltaT;
 
-    nextTh = normalizeAngle(nextTh);
+            // ADD other condition of angles 
+            nextX = prevX - dist*cos(prevTh);
+            nextY = prevY + dist*sin(prevTh);
+            nextTh = prevTh;
+        }
 
-    prevX = nextX;
-    prevY = nextY;
-    prevTh = nextTh;
+        nextTh = normalizeAngle(nextTh);
 
-    std::cout << "X', Y', Th': " << nextX << ", " << nextY << ", " << nextTh << std::endl;
-};    
+        prevX = nextX;
+        prevY = nextY;
+        prevTh = nextTh;
+
+        std::cout << "X', Y', Th': " << nextX << ", " << nextY << ", " << nextTh << std::endl;
+
+        return {nextX, nextY, nextTh};
+
+    };
+
+    std::vector<double> motionModelFx(double angVel, double linVel, double deltaT)
+    {
+
+
+        return {nextX, nextY, nextTh};
+
+    };
 
 };
 
@@ -109,6 +137,7 @@ void cbOdom(const nav_msgs::Odometry::ConstPtr &msg)
     double odomX = msg->pose.pose.position.x;
     double odomY = msg->pose.pose.position.y;
     std::cout << "odomX, odomY: " << odomX << ", " << odomY << std::endl;
+       
 }
 
 void cbLidarTest(const sensor_msgs::LaserScan::ConstPtr &msg)
@@ -187,8 +216,11 @@ int main(int argc, char** argv)
         double deltaT = currT - prevT;
         prevT = currT;
 
+        std::cout << "---" << std::endl;
+        std::cout << "rad, time: " << linVel/angVel << ", " << deltaT << std::endl;
+
         // Call the motion model and pass lin and angular vel
-        turtlebot->motionModel(angVel, linVel, deltaT);
+        std::vector<double> xyth = turtlebot->motionModel(angVel, linVel, deltaT);
         // turtlebot->motionModel();
 
         // Call subscriber callbacks once
