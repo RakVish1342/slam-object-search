1b1f25ff7013e42b2a7ff6c01a47366e0028b6d2
RakVish1342 <rakshith.vishwanatha@gmail.com>
2020-04-18 20:43:19 -0700

    Single aruco marker for EKF demo and report

--- catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/src/ekfTestMoving.cpp
+++ catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/src/ekfTestMoving.cpp
@@ -458,7 +458,7 @@ public:
             std::cout << ">>> MOVING..." << globalTStop << std::endl;
             // msg.linear.x = 0.5;
             // msg.angular.z = 0.25;
-            msg.linear.x = 0.5;
+            msg.linear.x = 0.0;
             msg.angular.z = 0.0;
 
         }
