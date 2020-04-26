d0eb6cc5fa91e30c3c7dfb322fc53328a6f9c020
RakVish1342 <rakshith.vishwanatha@gmail.com>
2020-04-26 04:06:42 -0700

    All three conditions working with camera data only.

--- catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/src/ekfTestMovingAruco.cpp
+++ catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/src/ekfTestMovingAruco.cpp
@@ -177,19 +177,27 @@ public:
             predictedStates(0) = states(0) + ( -r*sin(states(2)) + r*sin(states(2) + angVel*deltaT) );
             predictedStates(1) = states(1) + ( +r*cos(states(2)) - r*cos(states(2) + angVel*deltaT) );
             predictedStates(2) = states(2) + angVel*deltaT;
+
         }
         else
         {
-            // double dist = linVel*deltaT;
-            // // ADD other condition of angles 
-            // predictedStates(0) = states(0) - dist*cos(states(2));
-            // predictedStates(1) = states(1) + dist*sin(states(2));
-            // predictedStates(2) = states(2);
+            double dist = linVel*deltaT;
+            // ADD other condition of angles 
+            predictedStates(0) = states(0) - dist*cos(states(2));
+            predictedStates(1) = states(1) + dist*sin(states(2));
+            predictedStates(2) = states(2);
 
-            double r = linVel*deltaT;
-            predictedStates(0) = states(0) + ( -r*sin(states(2)) + r*sin(states(2) + angVel*deltaT) );
-            predictedStates(1) = states(1) + ( +r*cos(states(2)) - r*cos(states(2) + angVel*deltaT) );
-            predictedStates(2) = states(2) + angVel*deltaT;
+            // double r = linVel*deltaT;
+            // predictedStates(0) = states(0) + ( -r*sin(states(2)) + r*sin(states(2) + angVel*deltaT) );
+            // predictedStates(1) = states(1) + ( +r*cos(states(2)) - r*cos(states(2) + angVel*deltaT) );
+            // predictedStates(2) = states(2) + angVel*deltaT;
+
+            // std::cout << "++++++++" << std::endl;
+            // std::cout << r << std::endl;
+            std::cout << "++++++++" << std::endl;
+            std::cout << predictedStates << std::endl;
+            std::cout << "++++++++" << std::endl;
+            std::cout << states << std::endl;
 
         }
 
@@ -203,15 +211,11 @@ public:
             // std::cout << "X', Y', Th': " << states(0) << ", " << states(1) << ", " << states(2) << std::endl;
         }
 
-        // if(bTestMotionModelOnly)
-        // {
-            states = predictedStates;
-        // }        
+        // states = predictedStates;
+    // };
 
-    };
-
-    void motionModelVariance(double angVel, double linVel, double deltaT)
-    {
+    // void motionModelVariance(double angVel, double linVel, double deltaT)
+    // {
         double derivXTh, derivYTh;
     
         if (angVel > angVelThresh)
@@ -225,17 +229,17 @@ public:
         }
         else
         {
-            // double dist = linVel*deltaT;
-            // // ADD other condition of angles 
-            // derivXTh = dist*sin(states(2));
-            // derivYTh = dist*cos(states(2));
-            // // derivTh = 1 got from Identity addition
-
-            double r = linVel*deltaT;
-            // Derivative of X and Y wrt Th
-            derivXTh = -r*cos(states(2)) + r*cos(states(2) + angVel*deltaT);
-            derivYTh = -r*sin(states(2)) + r*sin(states(2) + angVel*deltaT);
+            double dist = linVel*deltaT;
+            // ADD other condition of angles 
+            derivXTh = dist*sin(states(2));
+            derivYTh = dist*cos(states(2));
             // derivTh = 1 got from Identity addition
+
+            // double r = linVel*deltaT;
+            // // Derivative of X and Y wrt Th
+            // derivXTh = -r*cos(states(2)) + r*cos(states(2) + angVel*deltaT);
+            // derivYTh = -r*sin(states(2)) + r*sin(states(2) + angVel*deltaT);
+            // // derivTh = 1 got from Identity addition
         }
 
         Eigen::MatrixXd tmp = Eigen::MatrixXd::Zero(numModelStates, numModelStates);
@@ -257,20 +261,11 @@ public:
             std::cout << predictedVariances << std::endl;
         }
 
-        // if(bTestMotionModelOnly)
-        // {
-            variances = predictedVariances;
-        // } 
+        states = predictedStates;
+        variances = predictedVariances;
 
     };
 
-
-
-    // void arucoAngle()
-    // {
-    //     std::cout << "ARUCOANGLE" << std::endl;
-
-    // }
 
     void cbAruco(const aruco_msgs::MarkerArray::Ptr &msg)
     {
@@ -295,15 +290,12 @@ public:
             aruco_msgs::Marker &marker_i = msg->markers.at(i);
             double x = marker_i.pose.pose.position.x;
             double z = marker_i.pose.pose.position.z;
-
             double avgRange = z;
             double headingMiddle = -std::atan2(x,z); // negated so that angle is positive to the LHS of robot
 
-    //// Begin For loop for each landmark
-
-            //?? Setting a landmark manually for now. Will need a loop actually
-            int landmarkId = 0;
+            int landmarkId = marker_i.id - 1; //Was written for landmark indexes starting from 0. But Aruco markers from idx 1 are being used.
             int stateIdx = (numModelStates-1) + (landmarkId+1);
+
             if(bAllDebugPrint)
             {
                 std::cout << "stateIdx = " << stateIdx << std::endl;
@@ -418,10 +410,7 @@ public:
                 std::cout << "UNSTABLE" << std::endl;
             }
 
-        }
-
-
-    //// End For loop for each landmark
+        } // End for each landmark
 
         states = predictedStates;
         variances = predictedVariances;
@@ -434,88 +423,6 @@ public:
         }
 
     };
-
-    // // static std::vector<double> lidarRangeHeading(const std::vector<float> &lidarRange, const double angleInc)
-    // std::vector<double> lidarRangeHeading(const std::vector<float> &lidarRange, const double angleInc)
-    // {
-    //     double avgRange = 0;
-    //     int ctr = 0;
-    //     for (int i = 0; i < lidarRange.size(); ++i)
-    //     {
-    //         if (lidarRange[i] < INF)
-    //         {
-    //             avgRange += lidarRange[i];
-    //             ctr += 1;
-    //         }
-    //     }
-    //     if(ctr <= 3) // Not enough number of reliable lidar reflections
-    //     {
-    //         avgRange = INF;
-    //     }
-    //     else
-    //     {
-    //         avgRange /= ctr;
-    //     }
-
-    //     double headingStart = 0;
-    //     double headingStop = 0;
-    //     double headingMiddle = 0;
-    //     for (int i = 0; i < lidarRange.size()-1; ++i)
-    //     {
-    //         if(lidarRange[i] >= INF && lidarRange[i+1] < INF)
-    //         {
-    //             headingStart = (i+1)*angleInc;
-    //             break;
-    //         }
-    //     }
-    //     headingStart = normalizeAngle(headingStart);
-
-    //     for (int i = 0; i < lidarRange.size()-1; ++i)
-    //     {
-    //         if(lidarRange[i] < INF && lidarRange[i+1] >= INF)
-    //         {
-    //             headingStop = i*angleInc;
-    //         }
-    //     }        
-    //     headingStop = normalizeAngle(headingStop);
-
-    //     // Heading points are in the positive half. Take avg and assign +ve sign
-    //     if ( (headingStart > 0) && (headingStart <= PI) && (headingStop > 0) && (headingStop <= PI) )
-    //     {
-    //         headingMiddle = std::abs(headingStop + headingStart) / 2.0;
-    //     }
-    //     // Heading points are in the negative half. Take avg and assign -ve sign
-    //     else if ( (headingStart < 0) && (headingStart >= -PI) && (headingStop < 0) && (headingStop >= -PI) )
-    //     {
-    //         headingMiddle = - std::abs(headingStop + headingStart) / 2.0;
-    //     }
-    //     // Start point is in positive half and stop is in the negative half. 
-    //     // Find actual measurement angle viz complement of direct angle. Half of this angle measurement to be added to start and normalized
-    //     else if ( (headingStart > 0) && (headingStart <= PI) && (headingStop < 0) && (headingStop >= -PI) )
-    //     {
-    //         double measurementAngle  = 2*PI - (std::abs(headingStart) + std::abs(headingStop));
-    //         headingMiddle = normalizeAngle( headingStart + measurementAngle/2.0 );
-    //     }
-    //     // Start point is in negative half and stop point is in the positive half
-    //     // Find angle range in between the start and stop points. Assign sign based on which is larger in magnitude
-    //     else if ( (headingStart < 0) && (headingStart >= -PI) && (headingStop > 0) && (headingStop <= PI) )
-    //     {
-    //         double measurementAngle  = std::abs(headingStart) + std::abs(headingStop);
-    //         if ( std::abs(headingStop) >= std::abs(headingStart) ) // Will fall on positive side
-    //         {
-    //             headingMiddle = + ( measurementAngle/2.0 - std::abs(headingStart) );
-    //         }
-    //         else // Will fall on negative side
-    //         {
-    //             headingMiddle = - ( measurementAngle/2.0 - std::abs(headingStop) );
-    //         }
-    //     }
-
-    //     std::vector<double> output = {avgRange, headingMiddle};
-    //     return output;
-    // }
-
-
 
     void tmpPrint(Eigen::VectorXd z, Eigen::VectorXd zHat, int stateIdx, double tmpAngle)
     {
@@ -536,170 +443,6 @@ public:
     }
 
 
-    // // Also behaves as the sensorModel()
-    // //OR instead of making it static, might be able to use: 
-    // //https://answers.ros.org/question/282259/ros-class-with-callback-methods/
-    // //turtle_lidar = n.subscribe("/scan", 10, &TurtleEkf::cbLidar, this);  --- In the constructor where the subscriber is initiated.
-    // // static void cbLidar(const sensor_msgs::LaserScan::ConstPtr &msg)
-    // void cbLidar(const sensor_msgs::LaserScan::ConstPtr &msg)
-    // {
-    //     double angleMin = msg->angle_min;
-    //     double angleInc = msg->angle_increment;
-    //     std::vector<float> lidarRange = msg->ranges;
-
-    //     std::vector<double> landmarkMeasurement = lidarRangeHeading(lidarRange, angleInc);
-    //     double avgRange = landmarkMeasurement[0];
-    //     double headingMiddle = landmarkMeasurement[1];
-
-    //     // avgRange = INF;
-    //     if( !(std::abs(avgRange) >= INF) )
-    //     {
-    //         if(bAllDebugPrint)
-    //         {
-    //             std::cout << "Predicted states before correction step = " << std::endl;
-    //             std::cout << predictedStates << std::endl;
-    //             std::cout << "Predicted variances before correction step = " << std::endl;
-    //             std::cout << predictedVariances << std::endl;        
-    //         }
-    // //// Begin For loop for each landmark
-
-    //         //?? Setting a landmark manually for now. Will need a loop actually
-    //         int landmarkId = 0;
-    //         int stateIdx = (numModelStates-1) + (landmarkId+1);
-    //         if(bAllDebugPrint)
-    //         {
-    //             std::cout << "stateIdx = " << stateIdx << std::endl;
-    //         }
-    //         if ( !bSeenLandmark(landmarkId) ) // If landmark not seen before, set the prior of that landmark to global position of the landmark
-    //         {
-    //             // NOTE: ujx is the state in states that corsp to this j-th landmark
-
-    //             // u_jx = u_tx + r*cos(phi + u_tth)
-    //             // landmark_x = robot_x + r*cos(phi + robot_heading)
-    //             predictedStates(stateIdx) = predictedStates(0) + avgRange * cos(headingMiddle + predictedStates(2));
-    //             predictedStates(stateIdx+1) = predictedStates(1) + avgRange * sin(headingMiddle + predictedStates(2));
-
-    //             if(bAllDebugPrint)
-    //             {
-    //                 std::cout << "ONE TIME STATES" << std::endl;
-    //                 std::cout << predictedStates << std::endl;
-    //             }
-
-    //             bSeenLandmark(landmarkId) = 1;
-    //         }
-
-    //         double delx = predictedStates(stateIdx) - predictedStates(0);
-    //         double dely = predictedStates(stateIdx+1) - predictedStates(1);
-    //         double q = delx*delx + dely*dely;
-    //         if(bAllDebugPrint)
-    //         {
-    //             std::cout << "delx, dely and q = " << std::endl;
-    //             std::cout << delx << std::endl;
-    //             std::cout << dely << std::endl;
-    //             std::cout << q << std::endl;
-    //         }
-
-    //         Eigen::VectorXd zj(2);
-    //         Eigen::VectorXd zjHat (2);
-    //         zj << avgRange, headingMiddle;
-    //         double tmpAngle = std::atan2(dely, delx) - predictedStates(2);
-    //         zjHat << std::sqrt(q) , normalizeAngle(tmpAngle);
-    //         if(bAllDebugPrint)
-    //         {
-    //             std::cout << "z and zHat = " << std::endl;
-    //             std::cout << zj << std::endl;
-    //             std::cout << zjHat << std::endl;
-    //         }
-
-    //         tmpPrint(zj, zjHat, stateIdx, tmpAngle);
-
-    //         Eigen::MatrixXd Fxj = Eigen::MatrixXd::Zero( (numModelStates + numComponents), numTotStates);        
-    //         Fxj.topLeftCorner(numModelStates,numModelStates) = Eigen::MatrixXd::Identity(numModelStates,numModelStates);
-            
-    //         Fxj(numModelStates, numModelStates + landmarkId) = 1;
-    //         Fxj(numModelStates+1, numModelStates + landmarkId+1) = 1;
-    //         if(bAllDebugPrint)
-    //         {
-    //             std::cout << "Fxj = " << std::endl;
-    //             std::cout << Fxj << std::endl;
-    //         }
-
-    //         // Partial differential of:
-    //         // zHat_x wrt modelX, modelY, modelTh, mx, my
-    //         // zHat_y wrt modelX, modelY, modelTh, mx, my
-    //         // H*z ==nt Cx or Hx in traditional state space model, but here input x for this step is estimated/expected sensor reading = zHat
-    //         Eigen::MatrixXd H (numComponents, numTotStates);
-    //         Eigen::MatrixXd Hq (numComponents, numTotStates);
-    //         H << 
-    //             -std::sqrt(q)*delx , -std::sqrt(q)*dely , 0    , std::sqrt(q)*delx   , std::sqrt(q)*dely ,
-    //             dely              , -delx               , -q   , -dely               , delx;        
-    //         Hq = (1/q) * H;
-    //         // H = H * (1/q);
-    //         // std::cout << "H = " << std::endl;
-    //         // std::cout << H << std::endl;
-
-    //         // (3+2)x(3+2n) = 5x5 for single landmark case, with each landmark being 2D
-    //         Eigen::MatrixXd HFxj (numTotStates, numTotStates);
-    //         Eigen::MatrixXd Htrans (numTotStates, numTotStates);
-    //         HFxj = Hq * Fxj;
-    //         if(bAllDebugPrint)
-    //         {
-    //             std::cout << "HFxj = " << std::endl; // Same as H since with only one obstacle, we get just F to be identity
-    //             std::cout << HFxj << std::endl;        
-    //         }
-
-    //         Eigen::MatrixXd K (numTotStates, numComponents);
-    //         Eigen::MatrixXd tmp (numComponents, numComponents);
-    //         Eigen::MatrixXd tmpInv (numComponents, numComponents);
-    //         // tmp = HFxj * predictedVariances * HFxj.transpose(); // MUST add process/gaussian noise so that in Correction step, 
-    //                                                             //matrix inversion does not yield inv(0) and thus Nan after sometime
-    //         Htrans = HFxj.transpose();
-    //         tmp = HFxj * predictedVariances * Htrans + QsensorCovar;
-    //         tmpInv = tmp.inverse(); // As explained in the doc (http://eigen.tuxfamily.org/dox-devel/cl ... 030f79f9da), mat.inverse() returns the inverse of mat, keeping mat unchanged:
-    //         std::cout << "tmp Det" << std::endl;
-    //         std::cout << tmp.determinant() << std::endl;            
-    //         std::cout << "tmpInv" << std::endl;
-    //         std::cout << tmpInv << std::endl;
-    //         K = predictedVariances * Htrans * tmpInv; 
-
-    //         if(bAllDebugPrint)
-    //         {
-    //             std::cout << "K = " << std::endl;
-    //             std::cout << K << std::endl;
-    //         }
-
-    //         if( std::abs(tmp.determinant()) > 0.0001 ) // 10 power -4
-    //         {
-    //             predictedStates = predictedStates + K * (zj - zjHat);
-    //             Eigen::MatrixXd Iden (numTotStates, numTotStates);
-    //             Iden = Eigen::MatrixXd::Identity(numTotStates, numTotStates);
-    //             predictedVariances = ( Iden - K*HFxj) * predictedVariances;
-    //         }
-    //         else
-    //         {
-    //             std::cout << "UNSTABLE" << std::endl;
-    //         }
-    // //// End For loop for each landmark
-
-    //     }
-    //     else
-    //     {
-    //         std::cout << ">>> No Obstacle in Lidar Range" << std::endl;
-    //     }
-
-    //     states = predictedStates;
-    //     variances = predictedVariances;
-
-
-    //     if(bAllDebugPrint)
-    //     {
-    //         std::cout << "Corrected states and variances" << std::endl;
-    //         std::cout << states << std::endl;
-    //         std::cout << variances << std::endl;
-    //     }
-
-    // };
-
     // OR instead of making it static, can use:
     //https://answers.ros.org/question/282259/ros-class-with-callback-methods/
     //turtle_odom = n.subscribe("/odom", 10, &TurtleEkf::cbOdom, this);  --- In the constructor where the subscriber is initiated.
@@ -709,7 +452,7 @@ public:
 
         double odomX = msg->pose.pose.position.x;
         double odomY = msg->pose.pose.position.y;
-        //??prt        
+
         std::cout << ">>> Actual Position (Gazebo frame)" << std::endl;
         std::cout << "GazebOdomX, GazebOdomY: " << odomX << ", " << odomY << std::endl;
     };
@@ -727,8 +470,6 @@ public:
         if(globalTStop > timeWaitGazebo && globalTStop < timeWaitGazebo + timeThresh)
         {
             std::cout << ">>> MOVING..." << globalTStop << std::endl;
-            // msg.linear.x = 0.5;
-            // msg.angular.z = 0.25;
             msg.linear.x = 0.25;
             msg.angular.z = 0.25;
 
@@ -748,13 +489,9 @@ public:
         double deltaT = currT - prevT;
         prevT = currT;
 
-        //??prt
-        // std::cout << "---" << std::endl;
-        // std::cout << "rad, time: " << linVel/angVel << ", " << deltaT << std::endl;
-
         // Call the motion model and pass lin and angular vel
         motionModel(msg.angular.z, msg.linear.x, deltaT);
-        motionModelVariance(msg.angular.z, msg.linear.x, deltaT);
+        // motionModelVariance(msg.angular.z, msg.linear.x, deltaT);
 
     };
 
